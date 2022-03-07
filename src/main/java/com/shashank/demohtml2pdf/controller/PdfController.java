eb20ac6385235f1e83d21ef81f8f2188c75462ac
shashank <mshashank@gmail.com>
2022-03-07 02:37:41 -0500

    table and charts pdf

--- src/main/java/com/shashank/demohtml2pdf/controller/PdfController.java
+++ src/main/java/com/shashank/demohtml2pdf/controller/PdfController.java
@@ -1,17 +1,40 @@
 package com.shashank.demohtml2pdf.controller;
 
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomElement;
+import com.gargoylesoftware.htmlunit.html.HtmlCanvas;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.javascript.host.Window;
+import com.gargoylesoftware.htmlunit.javascript.host.html.HTMLCanvasElement;
 import com.itextpdf.html2pdf.HtmlConverter;
+import com.shashank.demohtml2pdf.model.Row;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
 
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.util.*;
 
-@RestController
+@Controller
 public class PdfController {
 
+    @Autowired
+    TemplateEngine templateEngine;
+
+    @Autowired
+    ServletContext servletContext;
 
     @GetMapping("/download")
     public ResponseEntity<?> downloadTransformedPdf() throws IOException {
@@ -28,7 +51,7 @@ public class PdfController {
     }
 
 
-    @GetMapping(value = "/pdf", produces = MediaType.APPLICATION_PDF_VALUE)
+    @GetMapping(value = "/pdfView", produces = MediaType.APPLICATION_PDF_VALUE)
     public ResponseEntity<?> viewTransformedPdf() throws IOException {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         HtmlConverter.convertToPdf(getClass().getClassLoader().getResourceAsStream("test.html"),
@@ -38,5 +61,66 @@ public class PdfController {
 
         return ResponseEntity.ok()
                 .body(bytes);
+    }
+
+    @GetMapping(value = "/pdf/table", produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<?> viewTransformedTablePdf(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        ByteArrayOutputStream outputStream = null;
+
+        try {
+            outputStream = new ByteArrayOutputStream();
+            List<Row> rows = new ArrayList<>();
+            rows.add(Row.builder().col1("col11").col2("col12").col3("col13").col4("col14").build());
+            rows.add(Row.builder().col1("col21").col2("col22").col3("col23").col4("col24").build());
+            rows.add(Row.builder().col1("col21").col2("col22").col3("col33").col4("col34").build());
+            rows.add(Row.builder().col1("col21").col2("col22").col3("col43").col4("col44").build());
+
+            WebContext context = new WebContext(request, response, servletContext);
+            String chart = getChart(context);
+
+            context.setVariable("rows", rows);
+            context.setVariable("chart",chart );
+            String rowsHtml = templateEngine.process("test-table", context);
+
+            HtmlConverter.convertToPdf(rowsHtml,
+                    outputStream);
+
+            byte[] bytes = outputStream.toByteArray();
+
+            return ResponseEntity.ok()
+                    .body(bytes);
+
+        }finally{
+            outputStream.flush();
+        }
+    }
+
+    private String getChart(WebContext context ) throws IOException {
+        String chartHtml = templateEngine.process("test-chartsjs", context);
+        String chartPng = null;
+        WebClient webClient = null;
+        try {
+            webClient = new WebClient(BrowserVersion.CHROME);
+            webClient.getOptions().setJavaScriptEnabled(true);
+            webClient.getOptions().setCssEnabled(true);
+            webClient.getOptions().setThrowExceptionOnScriptError(false);
+            HtmlPage html = webClient.loadHtmlCodeIntoCurrentWindow(chartHtml);
+            webClient.waitForBackgroundJavaScript(20000);
+
+            Window window = html.getPage().getEnclosingWindow().getScriptableObject();
+            int i = 0;
+            do {
+                i = window.animateAnimationsFrames();
+            } while (i > 0);
+
+            for (DomElement elem : html.getPage().getElementsByTagName("canvas")) {
+                HTMLCanvasElement canvas = (HTMLCanvasElement) ((HtmlCanvas) elem).getScriptableObject();
+                chartPng = canvas.toDataURL("image/png");
+                break;
+            }
+            return chartPng;
+        }finally {
+            webClient.close();
+        }
     }
 }
---
+++ src/main/java/com/shashank/demohtml2pdf/model/Row.java
@@ -0,0 +1,14 @@
+package com.shashank.demohtml2pdf.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Row {
+
+    String col1;
+    String col2;
+    String col3;
+    String col4;
+}
